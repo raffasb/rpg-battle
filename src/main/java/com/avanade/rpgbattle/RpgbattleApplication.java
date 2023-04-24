e150d80524e68349834990a7dfef812cebdc6ee8
Rafael Barros dos Santos <r.barros.dos.santos@avanade.com>
2023-04-24 10:26:22 -0300

    Spring initializr

---
+++ src/main/java/com/avanade/rpgbattle/RpgbattleApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.rpgbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgbattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgbattleApplication.class, args);
+	}
+
+}
